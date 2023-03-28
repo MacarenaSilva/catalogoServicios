5edf5876936932fa2660cfff7acc2651d6a5fa2c
msilva <msilva@technologylatam.com>
2023-03-27 23:08:08 -0300

    segundo commit: version 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola mundo!');
+  res.send('¡Hola mundo! desde server macarena');
 });
 
 const PORT = 3000;
