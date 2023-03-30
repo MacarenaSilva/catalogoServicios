7fd922cd3502e9ec1d530c2bd6cf9af36f29cf07
msilva <msilva@technologylatam.com>
2023-03-29 23:41:17 -0300

    creando cambios con node js instalado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('probando el servidor externo node');
+  res.send('probando el servidor externo node a');
 });
 
 const PORT = 3000;
