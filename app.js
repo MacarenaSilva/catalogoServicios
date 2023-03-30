4b7a6af1195950eb324c21b60275bcb33673cf58
msilva <msilva@technologylatam.com>
2023-03-29 22:13:45 -0300

    probando jenkins automatico

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola mundo! desde server macarena y cristian OK');
+  res.send('¡Hola mundo! desde server macarena 123');
 });
 
 const PORT = 3000;
