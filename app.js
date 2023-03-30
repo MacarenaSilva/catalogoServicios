91de906fb09159e9e26155c4bb5a12ccd53c6bc3
msilva <msilva@technologylatam.com>
2023-03-29 23:29:42 -0300

    servidor x

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Buenas tardes, estoy probando CI');
+  res.send('probando el servidor externo');
 });
 
 const PORT = 3000;
